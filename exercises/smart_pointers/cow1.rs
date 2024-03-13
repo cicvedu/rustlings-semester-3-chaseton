ba8c33f77cd774b64c76497ad601b438c99136e8
chaseton <lliqingchao@163.com>
2024-03-13 12:00:50 +0000

    4th update

--- exercises/smart_pointers/cow1.rs
+++ exercises/smart_pointers/cow1.rs
@@ -12,7 +12,7 @@
 //
 // Execute `rustlings hint cow1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::borrow::Cow;
 
@@ -38,7 +38,7 @@ mod tests {
         let mut input = Cow::from(&slice[..]);
         match abs_all(&mut input) {
             Cow::Owned(_) => Ok(()),
-            _ => Err("Expected owned value"),
+            _ => panic!("Expected owned value"),
         }
     }
 
@@ -49,6 +49,8 @@ mod tests {
         let mut input = Cow::from(&slice[..]);
         match abs_all(&mut input) {
             // TODO
+            Cow::Borrowed(_) => {Ok(())},
+            _ => {panic!("Expected owned value")},
         }
     }
 
@@ -60,7 +62,8 @@ mod tests {
         let slice = vec![0, 1, 2];
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => panic!("rrr"),
         }
     }
 
@@ -72,7 +75,8 @@ mod tests {
         let slice = vec![-1, 0, 1];
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => panic!("rrr"),
         }
     }
 }
